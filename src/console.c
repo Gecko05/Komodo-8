04a57555de6d2db4445ca7f67bc1d5e76c8bbfd6
Gecko05 <jacacent@gmail.com>
2019-08-04 12:45:40 -0500

    First lua Demo was successful

--- src/console.c
+++ src/console.c
@@ -5,11 +5,32 @@ void K8_Draw(){
 }
 
 void K8_Update(){
-
+  //SSD1351_update();
 }
 
 void luaDemo(){
   lua_State *L;
-
   L = luaL_newstate();
+  luaL_openlibs(L);
+  char *script = "function dosum(a, b) \
+                  return a + b   \
+                  end";
+  int error = luaL_loadstring(L, script) || lua_pcall(L, 0, 0, 0);
+  if (error) {
+    SSD_PRINTF("%s", lua_tostring(L, -1));
+    lua_pop(L, 1);  /* pop error message from the stack */
+  }
+  else {
+    SSD_PRINTF("No error");
+    lua_getglobal(L, "dosum");
+    lua_pushinteger(L, 2);
+    lua_pushinteger(L, 3);
+    lua_call(L, 2, 1);
+    int res;
+    res = luaL_checkinteger(L, 1);
+    SSD_PRINTF("\nResult: %i", res);
+  }
+
+  lua_close(L);
 }
+
